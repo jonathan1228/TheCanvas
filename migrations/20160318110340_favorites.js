ca851907b941cb12c50ec72cb39d694b85a22201
Jonathan <jonathankim1228@hotmail.com>
2016-03-28 08:26:07 -0700

    fixing heroku postgresql

--- migrations/20160318110340_favorites.js
+++ migrations/20160318110340_favorites.js
@@ -1,14 +1,8 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('favorites', function(table){
-  	table.increments();
-  	table.string('media');
-  	table.string('favoriteid');
-  	table.string('senderid');
-  	table.string('userid');
-  })
+  
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('favorites');
+
 };
